d88bb140829d7944ea6214cbe9db5a0019272a55
Emanuel Danci <danci.emanuel@gmail.com>
2018-05-10 21:44:06 +0300

    Remove .idea and old code

--- src/main/java/ro/uvt/mas/Messages.java
+++
@@ -1,8 +0,0 @@
-package ro.uvt.mas;
-
-public class Messages {
-    public final static String HELLO = "hello";
-    public final static String RUMOUR = "rumour";
-    public final static String INTRO = "introducting-";
-    public final static String THE_END = "the-end";
-}
